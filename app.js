21c5f07d9afbf68c6487b4527c125d3b285a1d83
Cydney Pollard <sincerelycydney087@gmail.com>
2023-01-12 22:45:04 -0500

    changed class to id for html tags; added JS logic

---
+++ app.js
@@ -0,0 +1,24 @@
+const score = document.getElementById("score");
+const wrongLetters = document.getElementById("wrongLetters");
+const guessesLeft = document.getElementById("guessesLeft");
+const words = document.getElementById("words");
+const hints = document.getElementById("hints");
+const correctWordSound = document.getElementById("correctWordSound");
+const incorrectWordSound = document.getElementById("incorrectWordSound");
+const word = document.getElementById("word");
+const underScores = document.getElementById("underScores");
+const hint = document.getElementById("hint");
+
+const keyboardLetters = document.getElementById("keyboardLetters");
+const playGame = document.getElementById("playGame");
+const loadGame = document.getElementById("loadGame");
+const startOver = document.getElementById("startOver");
+
+const winnerBox = document.getElementById("winnerBox");
+const correctWord = document.getElementById("correctWord");
+const nextWord = document.getElementById("nextWord");
+
+const gameOverBox = document.getElementById("gameOverBox");
+const numberOfChancesLeft = document.getElementById("numberOfChancesLeft");
+
+
